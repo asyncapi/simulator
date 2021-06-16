f530db70cb8458944016eed272a27df8ce121d9f
NektariosFifes <61620751+NektariosFifes@users.noreply.github.com>
2021-06-16 12:24:02 -0700

    Fixed issue with AsyncApiFile input verification.

--- Async-Simulator/src/bin/cli.js
+++ Async-Simulator/src/bin/cli.js
@@ -7,57 +7,58 @@ const filesystem = require('fs');
 const path = require('path');
 const  {asyncParser} = require('../parser/index');
 
-const filepathHandling = async (interface, file) => {
-  let filepath;
+const filepathHandling1 =  (rd, file) => {
+  const handlingContext = this;
+  handlingContext.rd = rd;
+  handlingContext.file = file;
 
-  const promtForFilepath = async () => {
-    interface.question('\nPlease provide a  filepath for a yaml asyncApiFile', (answr) => {
-      if (!String(answr).match(RegExp(/^.*\.(json|yaml)/, 'gm'))) {
-        interface.write('\nPlease provide a proper filepath ex:\'./myAsyncApi.json ./myAsyncApi.yaml\':\n');
-        promtForFilepath();
-      } else {
-        filesystem.stat(filepath, (err, stats) => {
-          if (err) {
-            interface.write('\nFile non Existent');
-            promtForFilepath();
-          } else {
-            return  answr;
-          }
-        });
-      }
+  const inputLoop = () => {
+    handlingContext.rd.question('\nEnter a proper asyncApi document filepath ',(answr) => {
+      filesystem.access(answr , 1 , (err) => {
+        if (err) {
+          handlingContext.rd.write(`\nError in accessing provided file \nDetails:${err}\n\n`);
+          inputLoop();
+        } else if (!String(answr).match(RegExp(/^.*\.(json|yaml)/, 'gm'))) {
+          handlingContext.rd.write('\nPlease provide a proper filepath ex:\'./myAsyncApi.json ./myAsyncApi.yaml\':\n');
+          inputLoop();
+        } else {
+          handlingContext.file = answr;
+          parseAsyncApi();
+        }
+      });
     });
   };
 
-  const handleFilepath = async (filepath = '') => {
-    const ParserInstance = await  asyncParser(filepath);
-    ParserInstance.Parse();
-    ParserInstance.mapAsyncApiToHandler();
-  };
-
-  console.log(chalk.blueBright(`
+  rd.write(chalk.blueBright(`
   Async api traffic simulator
   `));
+  rd.write('\nWelcome ');
+
   if (!!file) {
-    interface.write('\nWelcome to async api stress tester.');
     filesystem.access(file , 1 , (err) => {
       if (err) {
-        console.log(`\nError in accessing provided file \nDetails:${err}`);
-        file = promtForFilepath();
+        rd.write(`\nError in accessing provided file \nDetails:${err}\n\n`);
+        inputLoop();
       }
     });
   } else {
-    interface.question('Welcome to async api stress tester\nPlease provide a' +
-        ' json or yml file with the api.\n',(path = '') => {
-      file = path;
-    });
-    filesystem.access(file , 1 , (err) => {
-      if (err) {
-        console.log(`\nError in accessing provided file \nDetails:${err}`);
-        file = promtForFilepath();
-      }
-    });
+    rd.write('\nFilepath not provided');
+    inputLoop();
   }
-  await handleFilepath(await file);
+
+  function getFile () {
+    return handlingContext.file;
+  }
+
+  function parseAsyncApi() {
+    const parser = asyncParser(handlingContext.file);
+    parser.Parse();
+    parser.mapAsyncApiToHandler().then((res) => {console.log(res);});
+  }
+  return {
+    getFile,
+    parseAsyncApi
+  };
 };
 const run = () => {
   program.version('0.0.1', 'v', 'async-api performance tester cli version');
@@ -74,7 +75,8 @@ const run = () => {
   });
 
   const options = program.opts();
-  filepathHandling(cliInterface, path.resolve(options.filepath));
+
+  filepathHandling1(cliInterface, path.resolve(options.filepath));
 
   cliInterface.on('SIGINT', () => {
     console.log('\nShutting down');
