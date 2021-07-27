0eb42620d9584769b762c5ac9030a4d9cec18b0c
NektariosFifes <61620751+NektariosFifes@users.noreply.github.com>
2021-07-27 22:44:53 +0300

    fix empty block statement.

--- src/RequestHandler/HandlerFactory.js
+++ src/RequestHandler/HandlerFactory.js
@@ -1,11 +1,5 @@
 const {mqttHandler} = require('./mqttHandler');
 async function  HandlerFactory  (serverDat,operations,opts = {logging: false,saveLogs: false}) {
-  if (opts.logging === true) {
-
-  }
-  if (opts.saveLogs === true) {
-
-  }
   if (serverDat.protocol === 'mqtt') {
     return  await mqttHandler(serverDat,operations);
   }
