ff5f9ec9c8f433be4e1ffdb2b557e8d36b53f3ab
NektariosFifes <61620751+NektariosFifes@users.noreply.github.com>
2021-07-29 16:50:06 +0300

    added message for unsupported Protocol

--- src/RequestHandler/HandlerFactory.js
+++ src/RequestHandler/HandlerFactory.js
@@ -3,6 +3,7 @@ async function  HandlerFactory  (serverDat,operations= {logging: false,saveLogs:
   if (serverDat.protocol === 'mqtt') {
     return  await mqttHandler(serverDat,operations);
   }
+  console.log(`\n${serverDat.protocol} protocol is not supported.\nList of supported protocols:\n•mqtt`);
 }
 
 module.exports = {HandlerFactory};
