e21a70a7849398ec0adc411c2da11030ae2bec62
NektariosFifes <61620751+NektariosFifes@users.noreply.github.com>
2021-08-12 00:10:24 +0300

    fix

--- src/parser/tests/parser.test.js
+++ src/parser/tests/parser.test.js
@@ -1,24 +1,22 @@
 const assert = require('assert');
-const chai = require('chai');
 const expect = require('chai').expect;
 const path = require('path');
-const expectedOutputS = require('./expectedOutputs');
-const {GenerateOperations} = require('../GenerateOperations');
+const expectedOutputs = require('./expectedOutputs');
 const {parseFiles} = require('../parseFiles');
 describe('Parser Tests',function() {
-  let asyncApi,scenario;
+  let asyncApi;
   it('1# Should be able to parse correctly formatted asyncApi and scenario files.',async function() {
     try {
-      [asyncApi, scenario] = await parseFiles(path.resolve(__dirname, './correctFiles/CorrectlyFormattedAsyncApi.yaml'), path.resolve(__dirname, './correctFiles/CorrectlyFormattedScenario.yaml'));
+      [asyncApi] = await parseFiles(path.resolve(__dirname, './correctFiles/CorrectlyFormattedAsyncApi.yaml'), path.resolve(__dirname, './correctFiles/CorrectlyFormattedScenario.yaml'));
     } catch (err) {
       assert.fail('Test failed. Parser was not able to parse one of the files you provided');
     }
-    expect(asyncApi._json).to.deep.include(expectedOutputS.P1_AsyncApi._json);
+    expect(asyncApi._json).to.deep.include(expectedOutputs.P1_AsyncApi._json);
   });
   it('2# Throws Error when parsing incorrectly formatted AsyncApi file.',async function () {
     let Error = null;
     try {
-      const [asyncApi, scenario] = await parseFiles(path.resolve(__dirname, './wrongFiles/IncorrectlyFormattedAsyncApi.yaml'), path.resolve(__dirname, './correctFiles/IncorrectlyFormatedScenario.yaml'));
+      await parseFiles(path.resolve(__dirname, './wrongFiles/IncorrectlyFormattedAsyncApi.yaml'), path.resolve(__dirname, './correctFiles/IncorrectlyFormatedScenario.yaml'));
     } catch (err) {
       Error = err;
     }
