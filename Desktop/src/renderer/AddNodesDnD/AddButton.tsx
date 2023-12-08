735578ca988bca560a544131632d67c6b0bb97d1
SumantxD <st72365@gmail.com>
2023-12-08 14:52:20 +0530

    fixing sonar cloud error

--- Desktop/src/renderer/AddNodesDnD/AddButton.tsx
+++ Desktop/src/renderer/AddNodesDnD/AddButton.tsx
@@ -60,7 +60,7 @@ function DropdownMenu({ nodes, setNodes }) {
 
     function DropdownItem(props) {
         return (
-            <div className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+            <div className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)} onKeyDown={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
                 <span className="icon-button">{props.leftIcon}</span>
                 {props.children}
                 <span className="icon-right">{props.rightIcon}</span>
